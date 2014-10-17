410247cdc27fe989328960042187459a0ece87b4
seerey <seerey@users.noreply.github.com>
2014-10-17 15:49:22 -0400

    changes

--- devboot/js/offcanvas.js
+++ devboot/js/offcanvas.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
   $('[data-toggle="offcanvas"]').click(function () {
     $('.row-offcanvas').toggleClass('active')
   });
-
-  FastClick.attach(document.body);
 });
+
+window.addEventListener('load', function() {
+    FastClick.attach(document.body);
+}, false);
