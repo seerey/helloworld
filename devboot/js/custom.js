45254963b98592d5db46229ab337f50b91a96429
seerey <seerey@users.noreply.github.com>
2014-10-31 12:54:30 -0400

    changes

--- devboot/js/custom.js
+++ devboot/js/custom.js
@@ -1,12 +1,4 @@
 $(document).ready(function () {
-  // $('h4.panel-title').click(function (event) {
-  //   //alert("hello");
-  //   var test = $(this).children("a:first");
-  //   //test.text("hello");
-  //   event.stopPropagation();
-  //   test.trigger("click");
-  //   //$(this).find("a:first")[0].trigger("click");
-  //
-  // });
-  
+
+
 });
